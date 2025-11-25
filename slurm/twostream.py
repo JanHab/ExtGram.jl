c2fd1d4fdc212c91c3b2627ff4536fc8f4b17ae2
wj564106 <wj564106@login23-3.hpc.itc.rwth-aachen.de>
2025-11-25 16:29:48 +0100

    fixed clusterrun for twostream instability

--- slurm/twostream.py
+++ slurm/twostream.py
@@ -8,5 +8,5 @@ base_tree_level = 8
 polydeg = 3
 
 for M in M_vector:
-    command = f"julia examples/TwoStreamInstability_clusterrun.jl --threads={threads} --M={M} --closure={closure} --T_end={T_end} --base_tree_level={base_tree_level} --polydeg={polydeg}"
+    command = f"julia examples/TwoStreamInstability_clusterrun.jl {M} {closure} {T_end} {base_tree_level} {polydeg} --threads={threads}"
     createsbatch(command, nproc=threads, nnodes=1, time='00:15:00')
