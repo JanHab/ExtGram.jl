8b751815c9508974e0f3b1ea719d297dcfb6aa36
JanHab <jan.habscheid@rwth-aachen.de>
2025-11-28 15:38:01 +0100

    some extension for cluster runs

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --job-name=out/VlasovPoisson/TwoStreamInstability/slurm_output_closureExtGram_T25.0_M15_p3_level8.out
-#SBATCH --output=out/VlasovPoisson/TwoStreamInstability/slurm_output_closureExtGram_T25.0_M15_p3_level8.out
+#SBATCH --job-name=out/VlasovPoisson/LandauDamping/slurm_output_closureExtGram_T25.0_M25_p3_level6.out
+#SBATCH --output=out/VlasovPoisson/LandauDamping/slurm_output_closureExtGram_T25.0_M25_p3_level6.out
 #
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ export PATH=$PATH:/home/$USER/bin
 
 module load Julia
 
-julia examples/TwoStreamInstability_clusterrun.jl 15 ExtGram 25.0 8 3
+julia examples/LandauDamping_clusterrun.jl 25 ExtGram 25.0 6 3
--- slurm/BGK_convergence.py
+++ slurm/BGK_convergence.py
@@ -2,11 +2,11 @@ from clusterrun import createsbatch
 import os
 
 threads = 1
-N_vector = [10, 50, 100, 200, 500, 1_000]
+N_vector = [10, 50, 100, 200]#!, 500, 1_000]
 T_end = 0.2
 base_tree_level = 8
 polydeg = 1
-Kn = 1.0
+Kn = 0.1
 
 nnodes = 1
 time = '04:00:00'
