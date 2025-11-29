64c86c751792d4a068f2a6323773ff0f59e68526
JanHab <Jan.Habscheid@web.de>
2025-11-29 18:03:41 +0100

    integrate zero source

--- slurm/RiemannMoments.py
+++ slurm/RiemannMoments.py
@@ -2,10 +2,10 @@ from clusterrun import createsbatch
 import os
 
 # Solver and simulation parameters
-threads = 1
 M = 4
 closure = "ExtGram"
 Kn = 1.0
+source = "zero_source"#!"relaxation_source"
 T_end = 0.3
 base_tree_level = 6
 polydeg = 3
@@ -19,15 +19,16 @@ v_R = 0.0
 theta_R = 1.0
 
 # SLURM job parameters
+threads = 1
 nnodes = 1
 time = '04:00:00'
 memory_request = '16G'
 
-command = f"julia examples/RiemannMoments.jl {M} {closure} {Kn} {T_end} {base_tree_level} {polydeg} {rho_L} {v_L} {theta_L} {rho_R} {v_R} {theta_R}"
+command = f"julia examples/RiemannMoments.jl {M} {closure} {Kn} {source} {T_end} {base_tree_level} {polydeg} {rho_L} {v_L} {theta_L} {rho_R} {v_R} {theta_R}"
 os.system(command)
 # createsbatch(
 #     command, 
 #     nproc=threads, nnodes=nnodes, 
 #     time=time, mem=memory_request, 
-#     output_file=f"out/Riemann1D/Moments/relaxation/slurm_output__M{M}_closure{closure}_Kn{Kn}_T{T_end}_level{base_tree_level}_p{polydeg}_rho_L{rho_L}_v_L{v_L}_theta_L{theta_L}_rho_R{rho_R}_v_R{v_R}_theta_R{theta_R}.out"
+#     output_file=f"out/Riemann1D/Moments/relaxation/slurm_output__M{M}_closure{closure}_Kn{Kn}_source{source}_T{T_end}_level{base_tree_level}_p{polydeg}_rho_L{rho_L}_v_L{v_L}_theta_L{theta_L}_rho_R{rho_R}_v_R{v_R}_theta_R{theta_R}.out"
 # )
