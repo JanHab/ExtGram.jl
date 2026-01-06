5960f26a941285b1d1b7dc96438808554d06c13a
JanHab <Jan.Habscheid@web.de>
2026-01-06 12:00:31 +0100

    update vis

--- slurm/CompareMethods.py
+++ slurm/CompareMethods.py
@@ -9,7 +9,7 @@ closure_vec = ["Gram", "Grad"] # "ExtGram"
 Kn = 1.0
 source = "relaxation_source"
 T_end = 0.3
-base_tree_level = 6
+base_tree_level = 8
 polydeg = 1
 
 # Initial condition parameters for the Riemann problem
