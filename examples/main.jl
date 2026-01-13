895ee6cc5c2a967880572760a8ed3cca8fddd5c5
JanHab <jan.habscheid@rwth-aachen.de>
2026-01-13 16:30:46 +0100

    some settings for Rankine-Hugoniot

--- examples/main.jl
+++ examples/main.jl
@@ -5,19 +5,19 @@ end # Runs in environment setup
 using HyQMOM, Trixi, OrdinaryDiffEq, Plots, CSV, Tables
 
 # Parameter
-M = 8    # number of moments
+M = 12    # number of moments
 closure = "ExtGram" # flag for closure: "Gram", "ExtGram", "Grad"
 Kn = 1.0  # Knudsen number
-T_end = 1.0 #!0.3
-source = zero_source # * relaxation_source
+T_end = 25.0 #!0.3
+source = relaxation_source
 
 # Domain and discretization parameters
 # !x_lower = -2.0; x_upper = 2.0
-x_lower = -15.0; x_upper = 15.0
+x_lower = -20.0; x_upper = 100.0
 # x_lower = -50; x_upper = 50
 domain = (x_lower, x_upper)
 polydeg = 1  # polynomial degree
-base_tree_level = 7 #!8  # initial mesh refinement level
+base_tree_level = 8 #!8  # initial mesh refinement level
 surface_flux = flux_lax_friedrichs
 volume_flux = flux_central
 
@@ -29,17 +29,30 @@ volume_flux = flux_central
 # θ_R = 1.0
 
 # Rankine-Hugoniot conditions for shock tube
+# ρ_L = 1.0
+# θ_L = 1.0
+# p_L = ρ_L * θ_L
+# Ma = 1.0001
+# γ = 5.0/3.0
+# ρ_R = ρ_L * (Ma^2 * (γ+1)) / (2 + Ma^2 * (γ - 1))
+# p_R = p_L * (1 - γ + 2 * γ * Ma^2) / (1 + γ)
+# θ_R = p_R / ρ_R
+# c_L = sqrt(γ * p_L / ρ_L) # speed of sound left
+# v_L = Ma * c_L
+# v_R = v_L * ρ_L / ρ_R
+
+# Cai
+Ma = 1.4
+
 ρ_L = 1.0
+ρ_R = 2*Ma^2 / (Ma^2 + 1)
+
+v_L = sqrt(3) * Ma
+v_R = sqrt(3) / 2 * (Ma^2 + 1) / Ma
+
 θ_L = 1.0
-p_L = ρ_L * θ_L
-Ma = 8.0
-γ = 5.0/3.0
-ρ_R = ρ_L * (Ma^2 * (γ+1)) / (2 + Ma^2 * (γ - 1))
-p_R = p_L * (1 - γ + 2 * γ * Ma^2) / (1 + γ)
-θ_R = p_R / ρ_R
-c_L = sqrt(γ * p_L / ρ_L) # speed of sound left
-v_L = Ma * c_L
-v_R = v_L * ρ_L / ρ_R
+θ_R = (3*Ma^2 - 1) * (Ma^2 + 1) / (4 * Ma^2)
+
 
 # Setting up everything
 equations = GramianMomentEquations1D(M, Kn, closure)
@@ -56,10 +69,10 @@ basis = LobattoLegendreBasis(polydeg)
 # #= crashes for p > 1, i.e. this does not help at all
 indicator_sc = IndicatorHennemannGassner(
     equations, basis,
-    alpha_max = 0.5, #!1.0,#*0.5,#0.1, #  α_max = 1.0 seems natural -> corresponds to pure first order FV (Gassner paper)
-    alpha_min = 0.001, #!0.01,#0.01,
+    alpha_max = 0.5, #  α_max = 1.0 seems natural -> corresponds to pure first order FV (Gassner paper)
+    alpha_min = 0.001,
     alpha_smooth = true, #* false, # smoothes with all neighboring indicators to remove numerical artifacts
-    variable = (u, eqns)->u[1]*u[3]#! *u[5]
+    variable = (u, eqns)->u[1]*u[3]
 ) # ? Seems to restrict to M>=4
 # `custom variable for smoothness detection?`
 
@@ -99,6 +112,22 @@ callbacks, summary_callback = callbacksGramianMomentEquations(
     name="gram_solution",
 )
 
+amr_controller = ControllerThreeLevel(
+    semi, indicator_sc;
+    base_level=base_tree_level-3,
+    med_level=base_tree_level, med_threshold=0.1,
+    max_level=base_tree_level+3, max_threshold=0.6
+)
+
+amr_callback = AMRCallback(
+    semi, amr_controller,
+    interval=5,
+    adapt_initial_condition=true,
+    adapt_initial_condition_only_refine=true
+)
+
+callbacks = CallbackSet(callbacks, amr_callback)
+
 
 
 #= solve =#
@@ -121,10 +150,18 @@ x, ρ, v, p, p1 = plot_ρ_v_p(sol, M, x_lower, x_upper)
 
 θ = p ./ ρ
 # Plot primitive variables at final time
-plot(x, ρ[:, end], label="ρ", lw=2)
-plot(x, v[:, end], label="v", lw=2)
-plot(x, p[:, end], label="p", lw=2)
-plot(x, θ[:, end], label="θ", lw=2)
+plot(x, (ρ[:, end] .- ρ_L) ./ (ρ_R - ρ_L), label="ρ", lw=2, xlim=(-10, 10))
+plot(x, (v[:, end] .- v_R) ./ (v_L - v_R), label="v", lw=2, xlim=(-10, 10))
+plot(x, (p[:, end] .- θ_L .* ρ_L) ./ (θ_R .* ρ_R - θ_L .* ρ_L), label="p", lw=2, xlim=(-10, 10))
+plot(x, (θ[:, end] .- θ_L) ./ (θ_R - θ_L), label="θ", lw=2, xlim=(-10, 10))
+
+pl = plot(xlim=(-10, 10), title="T=$(T_end)", size=(500,500), yticks=0:0.1:1);
+plot!(pl, x, (ρ[:, end] .- ρ_L) ./ (ρ_R - ρ_L), label="ρ", lw=2);
+plot!(pl, x, (v[:, end] .- v_R) ./ (v_L - v_R), label="v", lw=2);
+# plot!(pl, x, (p[:, end] .- θ_L .* ρ_L) ./ (θ_R .* ρ_R - θ_L .* ρ_L), label="p", lw=2)
+plot!(pl, x, (θ[:, end] .- θ_L) ./ (θ_R - θ_L), label="θ", lw=2);
+display(pl)
+
 
 display(p1)
 savefig(p1, "out/Riemann1D/ρ_v_p.pdf")
