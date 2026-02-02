74c74432391359f547da839df4e5a51c4f6c4931
JanHab <jan.habscheid@rwth-aachen.de>
2026-02-02 10:23:10 +0100

    maxwellian with zero mean

--- miscellaneous/TestConversions.jl
+++ miscellaneous/TestConversions.jl
@@ -10,25 +10,25 @@ Kn = 1.0
 source = relaxation_source
 
 # IC
-ρ1 = 0.4
-v1 = 0.0
-θ1 = 1.0
-# ρ2 = 0.6
-ρ2_vector = range(0.1, 4.0, length=200)
-v2 = 0.0
+ρ1 = 1.0
+# v1 = -0.5
+θ1 = 0.3
+ρ2 = 1.0
+# ρ2_vector = range(0.1, 4.0, length=200)
+# v2 = -v1
 # v2_vector = range(0.5, 4.0, length=200)
-θ2 = 1.0
+θ2 = 0.3
+v_shift = range(0.0, 3.0, length=200)
 
 equations = GramianMomentEquations1D(M, Kn, closure)
-
-f1 = Maxwellian(ρ1, v1, θ1); # Density, velocity, temperature
 
 ϵ_cons = Float64[]
 ϵ_prim = Float64[]
 # for v2 in v2_vector
-for ρ2 in ρ2_vector
-# for v2_shift in v2_vector
-    f2 = Maxwellian(ρ2, v2, θ2); # Shock in density, but not velocity, temperature initially
+# for ρ2 in ρ2_vector
+for v in v_shift
+    f1 = Maxwellian(ρ1, -v, θ1); # Density, velocity, temperature
+    f2 = Maxwellian(ρ2, v, θ2); # Shock in density, but not velocity, temperature initially
     conservative_moments_full = convective_moments(f1, Val(M+2)) .+ convective_moments(f2, Val(M+2))
     # conservative_moments_full = conservative_moments_full .+ SVector{M+2}(0.0, v2_shift, zeros(M)...)
     conservative_moments = conservative_moments_full[1:end-1] # Remove hidden truth
@@ -57,28 +57,29 @@ for ρ2 in ρ2_vector
 end
 
 plt = scatter(
-    xlabel="ρ2", ylabel="ϵᵣ", yscale=:log10,
+    xlabel="v_shift", ylabel="ϵᵣ", yscale=:log10,
     title="M=$M, Closure=$(closure)",
 );
-scatter!(plt, ρ2_vector, ϵ_cons, label="Conservative", color=:blue);
-scatter!(plt, ρ2_vector, ϵ_prim, label="Primitive", color=:red);
+scatter!(plt, v_shift, ϵ_cons, label="Conservative", color=:blue);
+scatter!(plt, v_shift, ϵ_prim, label="Primitive", color=:red);
 display(plt);
 
 
 f = Vector{Float64}[]
 c_range = range(-5.0, 5.0, length=250)
-ρ2_range = range(0.1, 4.0, length=5)
-for ρ2 in ρ2_range
-    f1 = Maxwellian(ρ1, v1, θ1); # Density, velocity, temperature
-    f2 = Maxwellian(ρ2, v2, θ2);
+# ρ2_range = range(0.1, 4.0, length=5)
+v_shift = range(0.0, 3.0, length=5)
+for v in v_shift
+    f1 = Maxwellian(ρ1, -v, θ1); # Density, velocity, temperature
+    f2 = Maxwellian(ρ2, v, θ2);
     push!(f, f1.(c_range) + f2.(c_range))
 end
 plt2 = plot(
     # c_range, f[1],
     xlabel="c", ylabel="f(c)",
-    title="Distribution functions for different ρ2",
+    title="Distribution functions for different v_shifts",
 );
 for i in 1:length(f)
-    plot!(plt2, c_range, f[i], label="ρ2=$(round(ρ2_range[i], digits=2))");
+    plot!(plt2, c_range, f[i], label="v_shift=$(round(v_shift[i], digits=2))");
 end
 display(plt2);
