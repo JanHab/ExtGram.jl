1241ae55e1c2c9e7759361a4d3e221358702ee8f
JanHab <Jan.Habscheid@web.de>
2026-02-16 19:05:36 +0100

    Gauge finer + DVM for ShockSTructure

--- examples/RiemannBGK.jl
+++ examples/RiemannBGK.jl
@@ -5,23 +5,30 @@ end # Runs in environment setup
 using ExtGram, Trixi, OrdinaryDiffEq, Plots, CSV, Tables, LinearAlgebra
 
 # Access arguments by index
-N = parse(Int, ARGS[1])
-c_l = parse(Float64, ARGS[2])
-c_u = parse(Float64, ARGS[3])
-Kn = parse(Float64, ARGS[4])
-source_string = ARGS[5]
+N = 500 #!parse(Int, ARGS[1])
+c_l = -10.0 #"parse(Float64, ARGS[2])
+c_u = 10.0#!parse(Float64, ARGS[3])
+Kn = 1.0 #!parse(Float64, ARGS[4])
+source_string = "relaxation_source" #!ARGS[5]
 source = source_string == "relaxation_source" ? relaxation_source : zero_source # default to zero_source if not relaxation_source
-T_end = parse(Float64, ARGS[6])
-base_tree_level = parse(Int, ARGS[7]) # e.g. 8
-polydeg = parse(Int, ARGS[8]) # e.g. 3
-ρ_L = parse(Float64, ARGS[9]) # 7.0
-v_L = parse(Float64, ARGS[10]) # 0.0
-θ_L = parse(Float64, ARGS[11]) # 1.0
-ρ_R = parse(Float64, ARGS[12]) # 1.0
-v_R = parse(Float64, ARGS[13]) # 0.0
-θ_R = parse(Float64, ARGS[14]) # 1.0
-x_left = parse(Float64, ARGS[15]) # -5.0
-x_right = parse(Float64, ARGS[16]) # 5.0
+T_end = 25.0 #!parse(Float64, ARGS[6])
+base_tree_level = 10 #!parse(Int, ARGS[7]) # e.g. 8
+polydeg = 1 #!parse(Int, ARGS[8]) # e.g. 3
+# ρ_L = parse(Float64, ARGS[9]) # 7.0
+# v_L = parse(Float64, ARGS[10]) # 0.0
+# θ_L = parse(Float64, ARGS[11]) # 1.0
+# ρ_R = parse(Float64, ARGS[12]) # 1.0
+# v_R = parse(Float64, ARGS[13]) # 0.0
+# θ_R = parse(Float64, ARGS[14]) # 1.0
+Ma = 2.0
+ρ_L = 1.0
+ρ_R = (2*Ma^2) / (Ma^2 + 1)
+v_L = sqrt(3) * Ma
+v_R = sqrt(3) / 2 * (Ma^2 + 1) / Ma
+θ_L = 1.0
+θ_R = (3*Ma^2 - 1) * (Ma^2 + 1) / (4 * Ma^2)
+x_left = -20.0 #!parse(Float64, ARGS[15]) # -5.0
+x_right = 100.0 #!parse(Float64, ARGS[16]) # 5.0
 domain = (x_left, x_right)
 
 # Setting up everything
@@ -101,6 +108,14 @@ x, ρ, v, θ, p, q = ρ_v_θ_p_BGK(semi, sol, equations)
 p1 = plot_ρ_v_p_bgk(ρ, v, p, x; xlims=(-2.0, 2.0))
 # display(p1)
 # savefig(p1, "out/Riemann1D/BGK/bgk_solution_N$(N)_c_l$(c_l)_c_u$(c_u)_Kn$(Kn)_source$(source_string)_T_end$(T_end)_rho_L$(ρ_L)_rho_R$(ρ_R)_v_L$(v_L)_v_R$(v_R)_theta_L$(θ_L)_theta_R$(θ_R)_base_tree_level$(base_tree_level)_polydeg$(polydeg)_x_left$(x_left)_x_right$(x_right)_rho_v_p.pdf")
+
+
+pl = plot(xlim=(-10, 10), title="T=$(T_end)", size=(500,500), yticks=0:0.1:1);
+plot!(pl, x, (ρ[:, end] .- ρ_L) ./ (ρ_R - ρ_L), label="ρ", lw=2, xlims=(-10, 10));
+plot!(pl, x, (v[:, end] .- v_R) ./ (v_L - v_R), label="v", lw=2, xlims=(-10, 10));
+# plot!(pl, x, (p[:, end] .- θ_L .* ρ_L) ./ (θ_R .* ρ_R - θ_L .* ρ_L), label="p", lw=2)
+plot!(pl, x, (θ[:, end] .- θ_L) ./ (θ_R - θ_L), label="θ", lw=2, xlims=(-10, 10));
+display(pl)
 
 # Store primitive variables in CSV file
 CSV.write(
